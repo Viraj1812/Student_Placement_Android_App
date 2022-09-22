47ac31577b16f9d1a0c15bdd386224b90054323a
Viraj <viraj.vasani1218@gmail.com>
2022-09-22 14:05:07 +0530

    database connected

--- app/src/main/java/com/example/studentplacementandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/studentplacementandroidapp/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.studentplacementandroidapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.studentplacementandroidapp.databaseConnection.Database
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Database()
+
     }
 }
